84c5e1476d088742818bb54bbb6a87af88ca6c69
Prathamesh Chanekar <prathamesh.chanekar@yestosystems.in>
2020-12-03 09:57:32 +0530

    first commit

---
+++ db.php
@@ -0,0 +1,9 @@
+<?php 
+
+$con = mysqli_connect("localhost","prachibarve","Prachi2014","dummyphptricks");
+    if (mysqli_connect_errno()){
+ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ die();
+ }
+ 
+ ?>
